638b39289ae079cddaff49eaa5003a39e2753261
Fox Tail <foxtail25@gmail.com>
2023-11-16 16:58:42 +0300

    step1

--- components/layout/header.js
+++ components/layout/header.js
@@ -2,10 +2,13 @@ import h from '../../styles/layout/lheader.module.scss'
 
 export default function Header() {
     return <header className={h.h_container}>
+
         <div className='width_container'>
+
             <div className={h.header}>
                 header
             </div>
+            
         </div>
     </header>
 }
